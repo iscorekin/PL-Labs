ad4daca94c29a9ccff10a778859d8e256fe3dd5e
Никита <nooblase@gmail.com>
2019-02-15 02:18:43 +0500

    lab 4 task 1

---
+++ Lab_4/Lab_4/Faculty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Faculty
+    {
+        public string Name { get; private set; }
+        public int Year { get; private set; }
+        public int Amount { get; private set; }
+        public int Cost { get; private set; }
+        
+
+        /// <param name="name">Название факультета</param>
+        /// <param name="year">Год основания</param>
+        /// <param name="amount">Количество студентов</param>
+        /// <param name="cost">Стоимость обучения</param>
+        public Faculty(string name, int year, int amount, int cost)
+        {
+            Name = name;
+            Year = year;
+            Amount = amount;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"Название: {Name}\nГод основания: {Year}\nКоличество студентов: {Amount}\nСтоимость обучения: {Cost}");
+        }
+    }
+}
---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faculty[] facs = new Faculty[5];
+            double averageCost = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                facs[i] = InputFaculty();
+                averageCost += facs[i].Cost;
+            }
+
+            averageCost /= 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (facs[i].Year >= 2000 && facs[i].Cost < averageCost)
+                    Console.WriteLine(facs[i]);
+            }
+        }
+
+        static Faculty InputFaculty()
+        {
+            string name;
+            int year, amount, cost;
+
+            Console.Write("Название: ");
+            name = Console.ReadLine();
+            Console.Write("Год основания: ");
+            year = int.Parse(Console.ReadLine());
+            Console.Write("Количество студентов: ");
+            amount = int.Parse(Console.ReadLine());
+            Console.Write("Стоимость обучения: ");
+            cost = int.Parse(Console.ReadLine());
+
+            return new Faculty(name, year, amount, cost);
+        }
+    }
+}
